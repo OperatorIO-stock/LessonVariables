66497a1f1a8a052a978bfe94fd8112148472b3a4
Casper <Casper@LAPTOP-1QQGAQ8P>
2024-10-03 17:50:59 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonsAgava
+{
+    internal class Player
+    {
+        static void Main(string[] args)
+        {
+            string namePlayer = "Steve";
+            char iconPlayer = '@';
+            int armor = 100;
+            float health = 100.0f;
+            double ammunition = 100000;
+        }
+    }
+}
